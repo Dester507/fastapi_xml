1597c5c39f1033beeb52608fdd336c8f9320c5b0
Anatolii <tolikdemchuk507@gmail.com>
2021-01-13 12:39:54 +0200

    Init

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from lxml import etree
+from lxml import etree as et
 
 app = FastAPI(title="Main Default API")
 rpc_api = FastAPI(title="Support RPC-XML API")
@@ -10,7 +10,8 @@ rpc_api = FastAPI(title="Support RPC-XML API")
 async def process_time_handler(request: Request, call_next):
     import methods
     try:
-        route = await request.json()
+        xml_str = await request.body()
+        route = await make_json(xml_str)
         response = await getattr(methods, route['method'])(**route["attrs"])
     except NameError:
         return JSONResponse(status_code=404, content="Method does not exist")
@@ -27,7 +28,33 @@ async def process_time_handler(request: Request, call_next):
         return JSONResponse(status_code=404, content="Bad response")
 
 
-async def make_json():
-    pass
+async def make_json(xml_str):
+    tree = et.XML(xml_str)
+    method_name = tree.xpath("//methodName")[0].text
+    params = tree.xpath("//params/param")
+    attrs = {}
+    for pa in params:
+        pa_name = pa.get('name')
+        pa_type = pa.getchildren()[0].getchildren()[0].tag
+        pa_attr = pa.getchildren()[0].getchildren()[0].text
+        attrs[pa_name] = await switch_type(pa_attr, pa_type)
+        print(attrs)
+    return {"method": method_name, "attrs": attrs}
+
+
+async def switch_type(param, type_pa):
+    try:
+        if type_pa == "str":
+            str(param)
+        elif type_pa == "int":
+            int(param)
+        elif type_pa == 'float':
+            float(param)
+        elif type_pa == "bool":
+            bool(param)
+        return param
+    except:
+        # JSONResponse 'Attr type error'
+        pass
 
 app.mount("/xml", rpc_api)
--- methods.py
+++ methods.py
@@ -4,11 +4,11 @@ from fastapi import Body
 from main import rpc_api
 
 
-@rpc_api.get("/hello")
+@rpc_api.post("/hello")
 async def hello_world():
     return {"msg": "Hello world!"}
 
 
-@rpc_api.get("/say_gg")
+@rpc_api.post("/say_gg")
 async def say_gg(name: str = Body(...), age: int = Body(...)):
     return {"msg": "gg", "name": name, "age": age}
